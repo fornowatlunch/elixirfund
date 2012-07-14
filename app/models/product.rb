f80768123822a7e95f66ececbc8adf31a776c576
Daniel <daniel.zukowski@gmail.com>
2012-07-13 18:38:37 -0600

    add partner and partner_id to accessible attributes on Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to :partner
-  attr_accessible :description, :image, :price, :title
+  attr_accessible :description, :image, :price, :title, :partner_id, :partner
 end
