7806f2392c42d028aa6f877a08d792d29ef55a3c
Daniel <daniel.zukowski@gmail.com>
2012-07-13 20:44:54 -0600

    partners admin index page

--- app/admin/partners.rb
+++ app/admin/partners.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Partner do
-  
+  filter :name, :label => 'Partner Name'
+  filter :city
+
+  index do
+    column 'ID' do |partner|
+      link_to partner.id, admin_partner_path(partner)
+    end
+    column :name
+    column :contact_name
+    column :phone
+    column "Address" do |partner|
+      partner.formatted_address
+    end
+    default_actions
+  end
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,8 @@
-class Partner < ActiveRecord::Base
-  attr_accessible :address1, :address2, :contact_email, :contact_name, :description, :fax, :image, :name, :phone
+class Partner < ActiveRecord::Base 
+  attr_accessible :address1, :address2, :city, :state, :zip_code, :contact_email, :contact_name, :description, :fax, :image, :name, :phone
+
+  def formatted_address
+    "#{self.address1}<br />#{self.city}, #{self.state} #{self.zip_code}".html_safe 
+  end
+  
 end
