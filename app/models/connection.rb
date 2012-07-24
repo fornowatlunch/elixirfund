3f6ccb144649f3252ca1129436b7d5380a356b43
Daniel <daniel.zukowski@gmail.com>
2012-07-23 20:23:58 -0600

    make some related models accessible

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,4 @@
 class Connection < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :can_manage, :to_user
+  attr_accessible :can_manage, :to_user, :user
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,5 +2,5 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   belongs_to :partner
-  attr_accessible :name, :price
+  attr_accessible :name, :price, :order, :product, :partner
 end
