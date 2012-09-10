57dbd5596911b0942d17a2e5cd320ee34c6767db
Chris Boertien <chris@aktionlab.com>
2012-09-10 16:25:09 -0600

    change devise session routes
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ ElixirFund::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users
+  devise_for :users, :path => :account, :skip => [:sessions]
+
+  as :user do
+    get   'login'  => 'devise/sessions#new',     :as => :new_user_session
+    post  'login'  => 'devise/sessions#create',  :as => :user_session
+    match 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session, :via => Devise.mappings[:user].sign_out_via
+  end
 
   root :to => "pages#show", :id => 'home'
 
