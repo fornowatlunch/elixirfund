078c0fc23a5a60f49a6761bcc7de0188e267aea0
Chris Boertien <chris@aktionlab.com>
2012-09-13 15:24:47 -0600

    update controller with more crud methods and change to using patient model
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,11 +1,32 @@
 class PatientsController < ApplicationController
-  load_and_authorize_resource :user, :parent => false
+  load_and_authorize_resource
 
   def index
-    @patients = @users
   end
 
   def show
-    @patient = @user
+  end
+
+  def new
+  end
+
+  def create
+    @patient.user = current_user
+    if @patient.save!
+      redirect_to account_path, :notice => t('label.patient_created')
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @patient.update_attribuptes params[:patient]
+      redirect_to patient_path(@patient), :notice => t('label.patient_updated')
+    else
+      render :edit
+    end
   end
 end
