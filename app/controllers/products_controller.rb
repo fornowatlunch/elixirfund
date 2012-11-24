ca6e21d8102de2bb3b9f3bfff4df181822724682
Patrick Vargas <patrick.vargas@colorado.edu>
2012-11-24 12:40:52 -0700

    Added styling for alerts.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
   load_and_authorize_resource
-  has_scope :page, :default => 1
-  
+
   def index
+    if params[:products][:search].nil?
+      @products = Product.where("title LIKE ?", "%" + params[:products][:search] + "%").page params[:page]
+    else
+      @products = @products.page params[:page]
+    end
   end
 
   def show
