f778f9e7407100c884c8d3d316402bc37794b41d
Daniel <daniel.zukowski@gmail.com>
2012-11-24 20:08:51 -0700

    created search for products with style

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
+require 'kaminari'
 class ProductsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    if !params[:products][:search].empty?
+      @products = Product.where("title LIKE ?", '%' + params[:products][:search] + '%').page params[:page]
+    else
+      @products = @products.page params[:page]
+    end
   end
 
   def show
