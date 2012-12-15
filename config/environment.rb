5ef793bd6560a72b6d383688c7818fad9ec7a9d5
Peter Klipfel <peter@klipfel.me>
2012-12-15 16:56:20 -0700

    moved actionmailer into environment.rb for staging

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ElixirFund::Application.configure do
+  config.action_mailer.default_url_options = { host: "example.com" }
+end
 # Initialize the rails application
 ElixirFund::Application.initialize!
