62556752b294f8bd3728f6393a64db10a43828e4
Daniel <daniel.zukowski@gmail.com>
2012-12-16 23:06:59 -0700

    hide cart link if not signed in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     presenter = klass.new(object, self)
     yield presenter
   end
+
+  def cart_link
+    if user_signed_in?
+      "<li>#{link_to t('label.cart'), cart_path}</li>" 
+    end
+  end
 end
