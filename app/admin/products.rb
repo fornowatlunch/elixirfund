3ece7730bf81cc12e96b46cc0246c97d7384f4f9
Daniel <daniel.zukowski@gmail.com>
2012-12-16 22:15:16 -0700

    product image uploader, admin area styling

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -4,13 +4,40 @@ ActiveAdmin.register Product do
   filter :price  
 
   index do
-    column "ID", :sortable => :id do |user|
-      link_to user.id, admin_user_path(user)
+    column "ID", :sortable => :id do |product|
+      link_to product.id, admin_product_path(product)
     end
-    column "Thumbnail", :image
-    column "Product Name", :title
+    column :image do |product|
+      image_tag product.image_url(:thumb) unless product.image.file.nil?
+    end
+    column :title
     column :description
     column :price
     default_actions
+  end
+
+  show do |product|
+    panel "Product Details" do
+      attributes_table_for product do
+        row :title
+        row :price
+        row :partner
+        row :image do
+          image_tag product.image_url(:thumb)
+        end
+        row :description
+      end
+    end
+  end
+
+  form(:html => { :multipart => true }) do |f|
+    f.inputs "Product Details" do
+      f.input :title
+      f.input :partner
+      f.input :image, :as => :file
+      f.input :description
+      f.input :price
+    end
+    f.buttons
   end
 end
