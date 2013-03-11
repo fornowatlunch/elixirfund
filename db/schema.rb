1c260a05e3b9285e3937cbb333dacda0b3bf9b9f
Daniel <daniel.zukowski@gmail.com>
2013-03-10 20:19:27 -0600

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303025357) do
+ActiveRecord::Schema.define(:version => 20130311011330) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
