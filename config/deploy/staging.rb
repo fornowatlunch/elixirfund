dbc10c78a1c84d3112ec3be6e5152e5f043b4417
Daniel Zukowski <daniel.zukowski@gmail.com>
2013-09-18 11:18:43 -0600

    increase kept releases to 3 for staging env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :rails_env, :staging # production or staging
 set :branch, rails_env
 set :deploy_to, "/var/www/#{application}/#{rails_env}"
-set :keep_release, 1 # 1 is good for staging, 2 or more is better for production
+set :keep_release, 3 # 1 is good for staging, 2 or more is better for production
