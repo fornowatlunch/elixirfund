c93db877d9ed18a85d055282b0ce2c7882ec2a6d
Daniel Zukowski <daniel.zukowski@gmail.com>
2015-01-16 13:49:08 -0700

    keep 5 production releases

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :rails_env, :production # production or staging
 set :branch, rails_env
 set :deploy_to, "/var/www/#{application}/#{rails_env}"
-set :keep_release, 1 # 1 is good for staging, 2 or more is better for production
+set :keep_release, 5 # 1 is good for staging, 2 or more is better for production
