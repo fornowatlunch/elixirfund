e7fc64eadb77490e56decd0ec02b766d339e805b
bhuwan <bhuwan@me.com>
2016-04-18 11:53:22 -0400

    Add Partner search capability

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,14 +57,13 @@ class ProductsController < ApplicationController
         if @zip < 10000 then
           @zip = "0#{@zip.to_s}"
         end
-        @partners.concat(Partner.where("title ILIKE ?", params[:products][:search]))
-
         @partners.concat(Partner.where("
           zip_code = ? OR zip_code = ? OR zip_code = ? OR zip_code = ? OR zip_code = ? OR zip_code = ?
           OR zip_code = ? OR zip_code = ? OR zip_code = ? OR zip_code = ? OR zip_code = ?",
                                        "#{@zip}", "#{@zip1}", "#{@zip2}", "#{@zip3}", "#{@zip4}", "#{@zip5}",
                                        "#{@zip6}", "#{@zip7}", "#{@zip8}", "#{@zip9}", "#{@zip10}").map { |p| p.id })
       end
+      @partners.concat(Partner.where("title ILIKE ?", params[:products][:search]))
       @products = Product.where("title ILIKE ? OR partner_id IN (?)", '%' + params[:products][:search] + '%', @partners.join(",").to_i).page(params[:page])
     else
       @products = Product.order(:created_at).page params[:page]
